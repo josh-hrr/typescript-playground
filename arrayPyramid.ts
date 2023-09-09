41a5ba61f5d6a72c19fc6f10c4218ba96d054ce6
josh-hrr <54568984+josh-hrr@users.noreply.github.com>
2023-09-08 21:35:10 -0600

    Create arrayPyramid.ts

---
+++ arrayPyramid.ts
@@ -0,0 +1,13 @@
+export function pyramid(n: number) {
+   
+  const result = [];
+  let subArray = []; 
+
+  for(let i = 1; i <= n; i++){
+    subArray.push(1);             
+    result.push([...subArray]); //REST operator creates a copy of subArray and push it into result
+  }
+
+  return result;
+  
+}
