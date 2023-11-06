64cf672b1d5b2c5a1fb21d2876fbf1f90b4ed16f
Josue Herrera <54568984+josh-hrr@users.noreply.github.com>
2023-11-06 17:40:04 -0600

    Update fakeBinary.ts

--- fakeBinary.ts
+++ fakeBinary.ts
@@ -1,3 +1,12 @@
+/*
+
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+Note: input will never be an empty string
+
+
+
+*/
+
 export const fakeBin = (x:string):string => {
   let xArray = x.split("");
   let xNewArray:string[] = [];
