8a51d881b49be4734607df7c23d66e4454deada1
Josue Herrera <54568984+josh-hrr@users.noreply.github.com>
2023-11-27 23:00:25 -0600

    Create swithIntToString.ts

---
+++ swithIntToString.ts
@@ -0,0 +1,41 @@
+export function switchItUp(intNumber:number):string {
+  
+  let result:string = "";
+  
+  switch(intNumber){
+      case 1:
+        result = "One"
+      break;
+      case 2:
+        result = "Two"
+      break;
+      case 3:
+        result = "Three"
+      break;
+      case 4:
+        result = "Four"
+      break;
+      case 5:
+        result = "Five"
+      break;
+      case 6:
+        result = "Six"
+      break;
+      case 7:
+        result = "Seven"
+      break;
+      case 8:
+        result = "Eight"
+      break;
+      case 9:
+        result = "Nine"
+      break; 
+      case 0:
+        result = "Zero"
+      break; 
+      default:
+        result = "invalid";
+  }
+  
+  return result;
+}
